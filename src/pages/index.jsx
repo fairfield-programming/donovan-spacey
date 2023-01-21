10174db722b9c9d9aad34f0184acbe397733a2e1
William McGonagle <wpmcgonagle@gmail.com>
2023-01-21 18:01:27 -0500

    Initial Commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,16 @@
+
+
+module.exports = () => {
+
+    return <main>
+        <div>
+            <h1>
+                {global.name}
+            </h1>
+            <p>
+                {global.description}
+            </p>
+        </div>
+    </main>
+
+}
