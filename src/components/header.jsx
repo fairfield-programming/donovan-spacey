f4509e5da6064b565132f939cd6b430b4cd8da7e
William McGonagle <wpmcgonagle@gmail.com>
2023-01-29 13:13:12 -0500

    Adding Styles

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 module.exports = function Header({  }) {
 
-    return <header>
+    return <header style="display: block; width: 100%; background: #f5f5f5;">
         <a href="/">{project.name}</a>
         <a href="/about">About</a>
         <a href="/search">Search</a>
